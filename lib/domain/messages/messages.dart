79f21b47aa626dfda456e7eaa7920c4d97070734
fcraveiro <dextruz@gmail.com>
2024-06-23 15:33:31 -0300

    Rebuild

--- lib/domain/messages/messages.dart
+++
@@ -1,6 +0,0 @@
-class MessagesError {
-  MessagesError._();
-
-  static String errorSupabase(String error) =>
-      "<b>Erro ao acessar o banco de dados dos Usuários!</b><br><p style=';margin-top: 2.5em; color: blue;'><br>Código : $error</p>";
-}
--- lib/screens/_widgets/phones.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import '../../components/text_styles/text_styles.dart';
-
-Widget widgetPhones(store) {
-  return Text(
-    store['phones'].join(' - '),
-    style: GFont().noticeDarkText(11.5),
-  );
-}
